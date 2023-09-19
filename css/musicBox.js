6a5cd2b524388cafb2497ace4f9f134cf0be5767
Kitcht <oteamton@gmail.com>
2023-09-19 08:52:41 +0700

    first commit

---
+++ css/musicBox.js
@@ -0,0 +1,25 @@
+const musicContainer = document.getElementById('music-container');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+const audio = document.getElementById('audio');
+
+
+
+// Play song
+function playSong() {
+	musicContainer.classList.add('play');
+	playBtn.querySelector('i.fas').classList.remove('fa-play');
+	playBtn.querySelector('i.fas').classList.add('fa-pause');
+  
+	audio.play();
+  }
+  
+  // Pause song
+  function pauseSong() {
+	musicContainer.classList.remove('play');
+	playBtn.querySelector('i.fas').classList.add('fa-play');
+	playBtn.querySelector('i.fas').classList.remove('fa-pause');
+  
+	audio.pause();
+  }
